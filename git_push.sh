13cb276717af3a300eb37ec87b183ae99f07b31f
Abhijeet <ak806252@gmail.com>
2024-10-01 21:55:33 +0530

    solved git_push.sh,repetitions.py

--- git_push.sh
+++ git_push.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-git add .  # Stage files
+git add .  # Stage all changes 
 
-sleep 3    # Introduce a 3-second delay
+# Get the list of staged filenames separated by commas
+file_list=$(git diff --cached --name-only | paste -sd ", ")
 
-num_files=$(git diff --cached --name-only | wc -l)
+# Construct the commit message
+commit_message="solved $file_list"
 
 # Commit the changes
-git commit -m "solved $num_files questions"
+git commit -m "$commit_message"
 
 # Push the changes to the master branch
-git push origin main
+git push origin master
