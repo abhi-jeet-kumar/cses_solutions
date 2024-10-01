9bb1b8cb42f41165fddf283f2034bd42d139c271
Abhijeet <ak806252@gmail.com>
2024-10-01 22:19:15 +0530

    solved some questions (and maybe told a joke): What is the best prefix for global variables? //

--- git_push.sh
+++ git_push.sh
@@ -4,7 +4,6 @@ git add .  # Stage all changes
 
 # Get a random programming joke from jokeapi.dev
 joke_json=$(curl -s -H "Accept: application/json" "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist")
-echo "Raw JSON: $joke_json"  # Print the raw JSON
 
 # Extract the joke based on the type (single or twopart) using jq
 joke_type=$(echo "$joke_json" | jq -r '.type')
@@ -16,8 +15,6 @@ else
   joke=$(echo "$joke_json" | jq -r '.joke')
 fi
 
-echo "Joke: $joke"  # Print the extracted joke
-
 # Construct the commit message
 commit_message="solved some questions (and maybe told a joke): $joke"
 echo "Commit message: $commit_message" # Print the commit message
@@ -26,4 +23,4 @@ echo "Commit message: $commit_message" # Print the commit message
 git commit -m "$commit_message"
 
 # Push the changes to the master branch
-git push origin master
+git push origin main
