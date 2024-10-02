bbbdc0b109282558abaaf4c1bbde8fd4d60de192
Abhijeet <ak806252@gmail.com>
2024-10-02 13:47:35 +0530

    solved some questions (and maybe told a joke): Two SQL tables sit at the bar. A query approaches and asks "Can I join you?"

--- number_spiral.py
+++ number_spiral.py
@@ -1,2 +1,13 @@
 # https://cses.fi/problemset/task/1071
 
+i = int(input())
+coords = []
+
+for x in range(i):
+    coords.append([int(j) for j in input().split()])
+    
+for coord in coords:
+    x = coord[0]
+    y = coord[1]
+    if x % 2 == 0:
+        print(x ** 2 - y)
