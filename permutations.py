1c640a3cb0228f31ffbcc80bb1cd9d7e9337b8cd
Abhijeet <ak806252@gmail.com>
2024-10-02 13:32:31 +0530

    solved some questions (and maybe told a joke): What do you get if you lock a monkey in a room with a typewriter for 8 hours? A regular expression.

--- permutations.py
+++ permutations.py
@@ -1,2 +1,14 @@
 # https://cses.fi/problemset/task/1070
 
+# the key to construct a beautiful construction is to first print out the even digits and then the odd digits.
+# for 1,2,3 no solution exists. 
+
+n = int(input())
+
+if n == 2 or n == 3:
+    print("NO SOLUTION")
+else:
+    for i in range(2, n+1, 2):
+        print(i, end=" ")
+    for j in range(1, n+1, 2):
+        print(j, end=" ")
