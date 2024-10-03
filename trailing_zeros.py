c038444bebca15e9d3c1a338c3a57d487ca94667
Abhijeet <ak806252@gmail.com>
2024-10-03 21:58:33 +0530

    solved some questions (and maybe told a joke): Why did the programmer quit his job? Because he didn't get arrays.

---
+++ trailing_zeros.py
@@ -0,0 +1,28 @@
+i = int(input())
+
+def factorial(num):
+    ans = 1
+    for x in range(1, num + 1):
+        ans *= x
+        
+    return ans
+
+fact = str(factorial(i))
+
+# 20
+# 2432902008176640000
+
+n = len(fact)
+if n <= 1:
+    print(n)
+else:
+    left_p, right_p = 0, 0
+    max_length = 0
+    while right_p < n:
+        if fact[left_p] == 0 and fact[right_p] == 0:
+            max_length = max(max_length, right_p - left_p + 1)
+            right_p += 1
+        else:
+            left_p = right_p
+    print(max_length)
+        
