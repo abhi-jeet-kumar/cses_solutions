a91d6835310eb55f58ef3aa9232a4f1d58ac74ea
Abhijeet <ak806252@gmail.com>
2024-10-03 23:13:51 +0530

    solved some questions (and maybe told a joke): How many programmers does it take to screw in a light bulb? None. It's a hardware problem.

--- trailing_zeros.py
+++ trailing_zeros.py
@@ -9,17 +9,4 @@ def factorial(num):
 
 fact = str(factorial(i))
 print(fact)
-# 20
-# 2432902008176640000
-
-n = len(fact)
-left_p, right_p = 0, 0
-max_length = 0
-while right_p < n:
-    if fact[left_p] == 0 and fact[right_p] == 0:
-        max_length = max(max_length, right_p - left_p + 1)
-        right_p += 1
-    else:
-        left_p = right_p
-print(max_length)
         
